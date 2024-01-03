004b0909d5de683c7079724d696d93de807e9fee
TkdTato <tatosarjveladzeoff@gmail.com>
2024-01-03 23:25:03 +0400

    asdf

--- src/app/components/facebook-chat/facebook-chat.component.ts
+++ src/app/components/facebook-chat/facebook-chat.component.ts
@@ -1,34 +1,34 @@
-// import { Component, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Component, ElementRef, Renderer2, OnInit } from '@angular/core';
 
-// @Component({
-//  selector: 'app-facebook-chat',
-//  template: `
-//     <div id="fb-root"></div>
-//     <div id="fb-customer-chat" class="fb-customerchat"></div>
-//  `,
-// })
-// export class FacebookChatComponent implements OnInit {
-//  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+@Component({
+ selector: 'app-facebook-chat',
+ template: `
+    <div id="fb-root"></div>
+    <div id="fb-customer-chat" class="fb-customerchat"></div>
+ `,
+})
+export class FacebookChatComponent implements OnInit {
+ constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-//  ngOnInit() {
-//     this.loadFacebookSdk();
-//  }
+ ngOnInit() {
+    this.loadFacebookSdk();
+ }
 
-//  private loadFacebookSdk() {
-//     const fbScript = this.renderer.createElement('script');
-//     fbScript.setAttribute('id', 'facebook-jssdk');
-//     this.renderer.setAttribute(
-//       fbScript,
-//       'src',
-//       'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'
-//     );
-//     this.renderer.appendChild(this.elementRef.nativeElement, fbScript);
+ private loadFacebookSdk() {
+    const fbScript = this.renderer.createElement('script');
+    fbScript.setAttribute('id', 'facebook-jssdk');
+    this.renderer.setAttribute(
+      fbScript,
+      'src',
+      'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'
+    );
+    this.renderer.appendChild(this.elementRef.nativeElement, fbScript);
 
-//     (window as any).fbAsyncInit = () => {
-//       FB.init({
-//         xfbml: true,
-//         version: 'v18.0',
-//       });
-//     };
-//  }
-// }
+    (window as any).fbAsyncInit = () => {
+      FB.init({
+        xfbml: true,
+        version: 'v18.0',
+      });
+    };
+ }
+}
